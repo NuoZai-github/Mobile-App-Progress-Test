ad45570dc2af749a68041f66c83c159e73dfb9e6
NuoZai-github <lime6199@gmail.com>
2025-11-25 13:06:04 +0800

    Basic Setup

---
+++ PT Mobile App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PT_Mobile_App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PT Mobile App/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PT_Mobile_App")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PT_Mobile_App.Pages")]
