ff0949177629c1c68378abf9d32a5ed656a96fab
NuoZai-github <lime6199@gmail.com>
2025-11-25 13:41:16 +0800

    Done Requirement

--- PT Mobile App/MauiProgram.cs
+++ PT Mobile App/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using CommunityToolkit.Maui; 
 
 namespace PT_Mobile_App
 {
@@ -9,6 +10,7 @@ namespace PT_Mobile_App
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit() 
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
@@ -16,7 +18,7 @@ namespace PT_Mobile_App
                 });
 
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
